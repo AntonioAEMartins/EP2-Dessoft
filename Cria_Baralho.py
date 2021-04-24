0f0832c5dce0efbf03b89ad201aba72edbd1a1d9
Antônio A E Martins <81169982+AntonioAEMartins@users.noreply.github.com>
2021-04-24 12:19:05 -0300

    Cria_Baralho

---
+++ Cria_Baralho.py
@@ -0,0 +1,13 @@
+def cria_baralho ():
+    import random
+    cartas=["2","3","4","5","6","7","8","9","10","J","Q","K","A"]
+    niepes=["♠","♥","♦","♣"]
+    baralho=[]
+    for e in niepes:
+        for i in cartas:
+            carta=i+e
+            baralho.append(carta)
+    return baralho
+
+
+print (cria_baralho())
