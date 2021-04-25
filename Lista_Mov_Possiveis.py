b71255b01d0f11f23f4ad89c3d16a3fa761c9a4a
Antônio A E Martins <81169982+AntonioAEMartins@users.noreply.github.com>
2021-04-25 12:11:08 -0300

    Lista_Movimentos_Possiveis

---
+++ Lista_Mov_Possiveis.py
@@ -0,0 +1,32 @@
+def extrai_naipe (carta):
+    if len(carta) == 3:
+        return carta[2]
+    else:
+        return carta[1]
+def extrai_valor (carta):
+    if len(carta) == 3:
+        return carta[0]+carta[1]
+    else:
+        return carta[0]
+
+
+def lista_movimentos_possiveis (b,p):
+    l_mov=[]
+    if p==0:
+        return []
+    if extrai_naipe(b[p])==extrai_naipe(b[p-1]):
+        l_mov.append(1)
+    elif extrai_valor (b[p])==extrai_valor(b[p-1]):
+        l_mov.append(1)
+    if extrai_naipe(b[p])==extrai_naipe(b[p-3]) and (p-3) >=0:
+        l_mov.append(3)
+    elif extrai_valor(b[p]) == extrai_valor(b[p-3]) and (p-3)>=0:
+        l_mov.append(3)
+    return l_mov
+
+    
+    
+
+L1=['A♦', 'J♥', 'Q♣', 'K♠', '10♣']
+print(lista_movimentos_possiveis(L1,4))
+
