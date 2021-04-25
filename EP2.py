ead8da7233c8501d381a4771551945b8490811e5
Antônio A E Martins <81169982+AntonioAEMartins@users.noreply.github.com>
2021-04-25 17:16:37 -0300

    1

--- EP2.py
+++ EP2.py
@@ -1,6 +1,5 @@
 #Import Bibliotecas
 import random
-from matl
 #Print Inicial
 #Criar Baralho
 def cria_baralho ():
@@ -13,48 +12,3 @@ def cria_baralho ():
             baralho.append(carta)
     random.shuffle(baralho)
     return baralho
-
-def extrai_naipe (carta):
-    if len(carta) == 3:
-        return carta[2]
-    else:
-        return carta[1]
-
-def extrai_valor (carta):
-    if len(carta) == 3:
-        return carta[0]+carta[1]
-    else:
-        return carta[0]
-def lista_movimentos_possiveis (b,p):
-    l_mov=[]
-    if p==0:
-        return []
-    if extrai_naipe(b[p])==extrai_naipe(b[p-1]):
-        l_mov.append(1)
-    elif extrai_valor (b[p])==extrai_valor(b[p-1]):
-        l_mov.append(1)
-    if extrai_naipe(b[p])==extrai_naipe(b[p-3]) and (p-3) >=0:
-        l_mov.append(3)
-    elif extrai_valor(b[p]) == extrai_valor(b[p-3]) and (p-3)>=0:
-        l_mov.append(3)
-    return l_mov
-
-def possui_movimentos_possiveis(baralho):
-    naipe=[]
-    valor=[]
-    for e in baralho:
-        naipe.append(extrai_naipe(e))
-        valor.append(extrai_valor(e))
-    estado=False
-    t=1
-    if naipe[t]==naipe[t-1]:
-        estado = True
-    elif valor[t]==valor[t-1]:
-        estado=True
-    while t<(len(baralho)):
-        if naipe[t]==naipe[t-3] and (t-3) >=0:
-            estado=True
-        elif valor[t] == valor[t-3] and (t-3)>=0:
-            estado=True
-        t+=1
-    return estado
