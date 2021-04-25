9a146c5f507e6174c8926ad392a52765d8577771
Antônio A E Martins <81169982+AntonioAEMartins@users.noreply.github.com>
2021-04-25 17:00:53 -0300

    EP2-Criar_Baralho

--- EP2.py
+++ EP2.py
@@ -1,1 +1,14 @@
-print ("Salves")
+#Import Bibliotecas
+import random
+from matl
+#Print Inicial
+#Criar Baralho
+def cria_baralho ():
+    cartas=["2","3","4","5","6","7","8","9","10","J","Q","K","A"]
+    niepes=["♠","♥","♦","♣"]
+    baralho=[]
+    for e in niepes:
+        for i in cartas:
+            carta=i+e
+            baralho.append(carta)
+    return baralho
